16122722c49214c12ffeec0e8f429edd592836b0
Roman Agapitov <agapitov820@gmail.com>
2023-03-11 21:06:50 +0500

    AddCode

---
+++ src/main/java/TicketManager/TicketManager.java
@@ -0,0 +1,41 @@
+package TicketManager;
+
+import java.util.Arrays;
+
+public class TicketManager {
+    TicketRepository repository;
+
+    public TicketManager(TicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Ticket ticket) {
+        repository.add(ticket);
+    }
+
+    public Ticket[] findAll(String depAir, String arrAir) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (matches(ticket, depAir, arrAir)) {
+
+                Ticket[] tmp = new Ticket[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    public boolean matches(Ticket ticket, String depAir, String arrAir) {
+        if (ticket.getDepAir().equals(depAir)) {
+            if (ticket.getArrAir().equals(arrAir)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
